8a0a5b988e80eb7b9d1fe1ec86936fe91ca90f59
ClareMcDonald <clare.s.mcdonald@gmail.com>
2023-08-06 18:44:45 -0700

    wrote three solutions to compare arrays: 1. nested loops, 2. object, 3. array methods

--- app.js
+++ app.js
@@ -77,3 +77,70 @@ function findPairWithSum(array) {
 
 console.log(findPairWithSum(numbersOne));
 console.log(findPairWithSum(numbersTwo));
+
+// Given 2 arrays, create a funciton that let's a user know (true/false) whether these two arrays contain any common items.
+// For example:
+// const array1 = ['a', 'b', 'c', 'x'];
+// const array2 = ['z', 'y', 'i'];
+// should return flase.
+// --------
+const array1 = ['a', 'b', 'c', 'x'];
+const array2 = ['z', 'y', 'x'];
+// should return true.
+
+// Brute force approach:
+// Time complexity: O(a * b)
+// Space complexity: O(1)
+function compareTwoArrays(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    for (let j = 0; j < array2.length; j++) {
+      if (array1[i] === array2[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+console.log(compareTwoArrays(array1, array2));
+
+// Better solution to impove time complexity:
+// Time complexity: O(a + b)
+// Space complexity: O(a)
+
+// array1 ==> obj {
+// a: true,
+// b: true,
+// c: true,
+// x: true
+// }
+
+// array2[index] ===  obj.properties
+
+function containsCommonItem(array1, array2) {
+  // loop through first array and create object where properties === items in the array
+  let map = {};
+  for (let i = 0; i < array1.length; i++) {
+    if (!map[array1[i]]) {
+      const item = array1[i];
+      map[item] = true;
+    }
+  }
+
+  // loop through second array and check if item in second array exists on created object.
+  for (let j = 0; j < array2.length; j++) {
+    if (map[array2[j]]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(containsCommonItem(array1, array2));
+
+// More readable solution:
+function containsCommonItem2(array1, array2) {
+  return array1.some((item) => array2.includes(item));
+}
+
+console.log(containsCommonItem2(array1, array2));
